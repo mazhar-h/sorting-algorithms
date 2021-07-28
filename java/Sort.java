725999a271de64dd20b218644d45644d02eaf7ad
mazyhar <mazhar.hossin1@gmail.com>
2021-07-28 18:10:22 -0500

    added new files moved algorithms into classes moved algorithms into files

--- java/Sort.java
+++ java/Sort.java
@@ -1,106 +1,27 @@
+package Java;
+import Java.MergeSort;
+import Java.QuickSort;
+
 public class Sort {
 	
 	public static void main(String[] args){
 		int[] array = {5, 3, 2, 4, 1};
+		int[] unsorted;
 		long startTime, endTime;
+		double time;
 		
+		unsorted = array.clone();
 		startTime = System.nanoTime();
-		mergesort(array);
+		QuickSort.sort(unsorted);
 		endTime = System.nanoTime();
-		System.out.printf("Mergesort running time: %d nanoseconds\n", (endTime - startTime));
-		
-		int[] temp = array.clone();
-		int high = temp.length - 1;
+		time = (double) (endTime - startTime) / 1000000000;
+		System.out.printf("Quick Sort running time: %.7fs\n", time);
+
+		unsorted = array.clone();
 		startTime = System.nanoTime();
-		quicksort(temp, 0, high);
+		MergeSort.sort(unsorted);
 		endTime = System.nanoTime();
-		System.out.printf("Quicksort running time: %d nanoseconds\n", endTime - startTime);
-	}
-	
-	private static int[] merge(int[] arrayA, int[] arrayB){
-		int[] arrayC = new int[arrayA.length+arrayB.length];
-		
-		int i = 0; // track arrayA's index
-		int j = 0; // track arrayB's index
-		int k = 0; // track arrayC's index
-		
-		//compare and merge
-		while ( i != arrayA.length && j != arrayB.length )
-		{
-			int a = arrayA[i];
-			int b = arrayB[j];
-			
-			if ( a < b )
-			{
-				arrayC[k] = a;
-				i++;
-			}
-			else {
-				arrayC[k] = b;
-				j++;
-			}
-			k++;
-		}
-		
-		//merge any trailing elements
-		while ( i != arrayA.length )
-			arrayC[k++] = arrayA[i++];
-		
-		while ( j != arrayB.length )
-			arrayC[k++] = arrayB[j++];
-		
-		return arrayC;
-	}
-	
-	public static int[] mergesort(int[] array){
-		int n = array.length;
-		
-		if ( n == 1 )
-			return array;
-		
-		int[] arrayOne = new int[n/2];
-		int[] arrayTwo = new int[n - arrayOne.length];
-		
-		for (int i = 0; i < arrayOne.length; i++)
-			arrayOne[i] = array[i];
-		
-		for (int i = 0; i < arrayTwo.length; i++)
-			arrayTwo[i] = array[(n/2)+i];
-		
-		arrayOne = mergesort(arrayOne);
-		arrayTwo = mergesort(arrayTwo);
-		
-		return merge(arrayOne, arrayTwo);
-	}
-	
-	private static int partition(int[] arr, int low, int high){
-		int pivot = high;
-		
-		//move elements larger than the pivot
-		//to the right of the pivot
-		while (low < pivot)
-		{
-			if ( arr[low] > arr[pivot] )
-			{
-				int temp = arr[low];
-				arr[low] = arr[pivot - 1];
-				arr[pivot - 1] = arr[pivot];
-				arr[pivot] = temp;
-				pivot--;
-			}
-			else
-				low++;
-		}
-		
-		return pivot;
-	}
-	
-	public static void quicksort(int[] arr, int low, int high){
-		if ( low < high)
-		{
-			int index = partition(arr, low, high);
-			quicksort(arr, low, index - 1);
-			quicksort(arr, index + 1, high);
-		}
+		time = (double) (endTime - startTime) / 1000000000;
+		System.out.printf("Merge Sort running time: %.7fs\n", time);
 	}
 }
--- python/sort.py
+++ python/sort.py
@@ -1,81 +1,26 @@
 import time
+import copy
+import quick_sort
+import merge_sort
 
-def merge(array_a, array_b):
-    array_c = []
-
-    #compare and merge
-    while len(array_a) > 0 and len(array_b) > 0:
-        a = array_a[0]
-        b = array_b[0]
-
-        if a < b:
-            array_c.append(a)
-            array_a.pop(0)
-        else:
-            array_c.append(b)
-            array_b.pop(0)
+def main():
+    array = [5,3,2,4,1]
     
-    #merge any trailing elements
-    while len(array_a) > 0:
-        a = array_a[0]
-        array_c.append(a)
-        array_a.pop(0)
+    unsorted = copy.deepcopy(array)
+    n = len(unsorted) - 1
+    start_time = time.perf_counter_ns()
+    quick_sort.sort(unsorted, 0, n)
+    end_time = time.perf_counter_ns()
+    running_time = (end_time - start_time) / 1000000000
+    print("quick sort running time: {:.7f}s".format(running_time))
 
-    while len(array_b) > 0:
-        b = array_b[0]
-        array_c.append(b)
-        array_b.pop(0)
-
-    return array_c   
-
-def mergesort(array):
-    n = len(array)
-
-    if n == 1:
-        return array
-
-    array_one = array[:int(n/2)]
-    array_two = array[int(n/2):n]
-
-    array_one = mergesort(array_one)
-    array_two = mergesort(array_two)
-
-    return merge(array_one, array_two)
-
-def partition(array, low, high):
-    pivot = high
-
-    #move elements larger than the pivot
-    #to the right of the pivot
-    while low < pivot:
-        if array[low] > array[pivot]:
-            temp = array[low]
-            array[low] = array[pivot-1]
-            array[pivot-1] = array[pivot]
-            array[pivot] = temp
-            pivot -= 1
-        else:
-            low += 1
-    
-    return pivot
-
-def quicksort(array, low, high):
-    if low < high:
-        index = partition(array, low, high)
-        quicksort(array, low, index-1)
-        quicksort(array, index+1, high)
+    unsorted = copy.deepcopy(array)
+    n = len(unsorted) - 1
+    start_time = time.perf_counter_ns()
+    merge_sort.sort(unsorted, 0, n)
+    end_time = time.perf_counter_ns()
+    running_time = (end_time - start_time) / 1000000000
+    print("merge sort running time: {:.7f}s".format(running_time))
 
 if __name__ == "__main__":
-    array = [5,3,2,4,1]
-
-    start_time = time.perf_counter_ns()
-    mergesort(array)
-    end_time = time.perf_counter_ns()
-    print("mergesort running time: {0} nanoseconds".format(end_time - start_time))
-
-    temp = array.copy()
-    n = len(array)-1
-    start_time = time.perf_counter_ns()
-    quicksort(array, 0, n)
-    end_time = time.perf_counter_ns()
-    print("quicksort running time: {0} nanoseconds".format(end_time - start_time))
+    main()
